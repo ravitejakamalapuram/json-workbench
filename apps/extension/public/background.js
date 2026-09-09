c8a428557a4c66249a6dba4da6f5041ee0cd1e70
raviteja kamalapuram <raviteja.kamalapuram@gmail.com>
2026-09-09 18:15:47 +0530

    feat: add extension service worker

---
+++ apps/extension/public/background.js
@@ -0,0 +1,5 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true }).catch(() => {
+    // Older Chrome versions may not expose the side-panel behavior API.
+  });
+});
