a4b1ac01a4b7d9d170cc4df8dae8338090ad3ca9
raviteja kamalapuram <raviteja.kamalapuram@gmail.com>
2026-09-09 18:49:51 +0530

    feat(core): expose scanner API

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,7 @@
 export * from "./types";
 export * from "./pipeline";
 export * from "./input";
+export * from "./scanner";
 export * from "./tokenizer";
 export * from "./structure";
 export * from "./profile";
